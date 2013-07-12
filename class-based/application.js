2c8b5d88a8dd1b1865efd50e5307ae0561683ed8
Martin Atzinov <mapanogg@gmail.com>
2013-07-12 14:44:48 +0300

    Updated class based application template

--- class-based/application.js
+++ class-based/application.js
@@ -9,31 +9,18 @@
 "use strict";
 
 /* 
-    Global Array containing all application instances;
-*/
-var APPLICATION_INSTANCES = window.APPLICATION_INSTANCES || [];
+    Global variables uppercase + underscore
 
-/* 
-    Global indexing variable
+    var GLOBAL_VARIABLE = 1;
 */
-var APPLICATION_NDEXES = window.APPLICATION_NDEXES || 0;
 
 function Application( autoInvoke /*, Arguments */ ) {
-
-    this.INSTANCE_INDEX = APPLICATION_NDEXES;
 
     if( autoInvoke ) {
 
         this.Init( /* Arguments */ )
 
     }
-
-    APPLICATION_INSTANCES.push({
-        instance: this,
-        index: APPLICATION_NDEXES
-    });
-
-    APPLICATION_NDEXES++;
 
     return this;
 }
@@ -56,13 +43,9 @@ Application.prototype = {
         Built-in destructor function.
         Destroy the current instance of the class.
     */
-    Destroy: function( instance ) {
+    Destroy: function( ) {
 
         /* Optional destruction code */
-
-        delete APPLICATION_INSTANCES[this.INSTANCE_INDEX];
-        APPLICATION_INSTANCES.splice(this.INSTANCE_INDEX-1, this.INSTANCE_INDEX);
-        APPLICATION_NDEXES--;
 
     }
 
